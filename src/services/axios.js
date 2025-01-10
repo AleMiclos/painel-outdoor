aa24df834ecc7f70af1f1fa93bd3516f5fbcca3f
AleMiclos <alexandremiclos@gmail.com>
2025-01-09 23:29:19 -0300

    att totems condominio/rota

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/', // Atualize com sua URL de backend
+  baseURL: 'https://outdoor-backend.onrender.com/', // Atualize com sua URL de backend
 });
 
 api.interceptors.request.use((config) => {
